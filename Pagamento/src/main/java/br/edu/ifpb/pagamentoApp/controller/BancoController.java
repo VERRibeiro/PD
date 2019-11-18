f07ebc9812a60b992bb15c55d86540b70a407c32
victor <victor.truesoft@gmail.com>
2019-11-18 13:01:13 -0300

    wip

--- Pagamento/src/main/java/br/edu/ifpb/pagamentoApp/controller/BancoController.java
+++ Pagamento/src/main/java/br/edu/ifpb/pagamentoApp/controller/BancoController.java
@@ -1,22 +1,28 @@
 package br.edu.ifpb.pagamentoApp.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import br.edu.ifpb.pagamentoApp.service.client.ClientSender;
 
-@RestController
+@Controller
 public class BancoController {
 	
 	@RequestMapping("/sendMessage")	
 	public String sendMessagem() {
 		ClientSender c = new ClientSender();
 		try {
-			c.sendMessage();
+			c.sendMessage("");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return "Enviado";
+	}
+	
+	@RequestMapping(value = "/form", method = RequestMethod.GET)
+	public String form() {				
+		return "form";
 	}
 }
--- Pagamento/src/main/java/br/edu/ifpb/pagamentoApp/service/client/ClientSender.java
+++ Pagamento/src/main/java/br/edu/ifpb/pagamentoApp/service/client/ClientSender.java
@@ -13,20 +13,24 @@ import br.edu.ifpb.pagamentoApp.model.Cartao;
 
 public class ClientSender {
 
-	public void sendMessage() throws IOException, TimeoutException {
+	public void sendMessage(String mensagem) throws IOException, TimeoutException {
 		ConnectionFactory connectionFactory = new ConnectionFactory();
 		connectionFactory.setHost("localhost");
 		connectionFactory.setPort(5672);
 		String NOME_FILA = "BANCO";
 		String[] bandeiras = {"VISA", "MASTER", "BRADESCO"};
 		Gson g = new Gson();
-		
+		int i = (int) (Math.random() * 3);
+		if(mensagem.isEmpty())
+		{			
+			Cartao c = new Cartao("005", "10", "9851-9590-5405-4563", bandeiras[i]);
+			mensagem = g.toJson(c);
+		}
 		try (Connection connection = connectionFactory.newConnection(); Channel canal = connection.createChannel();) {
 			
-			int i = (int) (Math.random() * 3);
-			canal.queueDeclare(NOME_FILA, true, false, false, null);
-			Cartao c = new Cartao("005", "10", "9851-9590-5405-4563", bandeiras[i]);
-			String mensagem = g.toJson(c);
+			
+			canal.queueDeclare(NOME_FILA, true, false, false, null);			
+			
 			System.out.println("[x] Enviado '" + mensagem + "'");
 			// â€‹(exchange, routingKey, mandatory, immediate, props, byte[] body)
 			canal.basicPublish("", NOME_FILA, MessageProperties.PERSISTENT_TEXT_PLAIN, mensagem.getBytes());
